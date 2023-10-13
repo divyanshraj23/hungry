2d2d6f2ab930f0a9f32123545441299edab815eb
divyanshraj23 <divyanshggu@gmail.com>
2023-10-13 19:48:03 +0530

    singhada

--- hungry.py
+++ hungry.py
@@ -1,7 +1,7 @@
 hungry=input("are you hungry")
 if hungry=="yes":
     print("eat samosa")
-    print("eat pizza")
+    print("eat singhada")
     print('eat burger')
 else:
     print("do your homework")
